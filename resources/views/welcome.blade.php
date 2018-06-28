5f0c5ab4d31ff5b3a23674bce25f3d8463c4c83f
Megly <ec2-user@ip-172-31-11-140.us-east-2.compute.internal>
2018-06-28 06:19:59 +0000

    megly love

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Megly love
                 </div>
 
                 <div class="links">
